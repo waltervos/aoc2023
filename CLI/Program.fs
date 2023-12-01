6b7d6bcebc348b340763f515a798b89636a51a6d
Walter Vos <walter.vos@ns.nl>
2023-12-01 14:59:08 +0100

    Solution day 1 part 1

---
+++ CLI/Program.fs
@@ -0,0 +1,29 @@
+﻿open System
+open System.IO
+
+let solution =
+    File.ReadAllLines "..\\Puzzles\\day-01.txt"
+    |> List.ofArray
+    |> List.map (fun (stringList) -> stringList |> Seq.toList)
+    |> List.map (fun charList ->
+        charList
+        |> List.fold
+            (fun total char ->
+                let charAsString = Char.ToString char
+
+                let charValue =
+                    match Int32.TryParse charAsString with
+                    | true, char -> charAsString
+                    | _ -> ""
+
+                total + charValue)
+            "")
+    |> List.fold
+        (fun total intString ->
+            let firstInt = intString[0] |> Char.ToString
+            let lastInt = intString |> Seq.toList |> List.last |> Char.ToString
+            
+            total + (firstInt + lastInt |> Int32.Parse))
+        0
+
+printfn "%A" solution
---
+++ Solutions/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace Solutions
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
